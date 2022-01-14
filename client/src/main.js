7562c5f6759ce737769b3f7a5bfaffcc52247189
Giovanni <giovi.cestaro@gmail.com>
2022-01-14 12:49:59 +0100

    last add

--- client/src/main.js
+++ client/src/main.js
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import List from '@/components/list.vue'
 import Edit from '@/components/Edit.vue'
 import Detail from '@/components/Detail.vue'
 import Creation from '@/components/Creation.vue'
+import VueRouter from 'VueRouter'
 
-import VueRouter from 'VueRouter';
 Vue.use(VueRouter)
 
 const routes =[
   {
-    path:'/',
+    path:'/list',
     components: List
   },
   {
-    path:'/',
+    path:'/edit',
     components: Edit
   },
   {
-    path:'/',
+    path:'/detail',
     components: Detail
   },
   {
-    path:'/',
+    path:'/creation',
     components: Creation
   },
   {
